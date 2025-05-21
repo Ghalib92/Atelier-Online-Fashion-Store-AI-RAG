6a75767d7df286999db4e6a9118d3a7fccddeea0
Ghalib92 <ghalibmohammad92@gmail.com>
2025-05-21 17:33:04 +0300

    Initial Commit

---
+++ Atelier_Fashion/Atelier_Fashion/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Atelier_Fashion project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Atelier_Fashion.settings')
+
+application = get_asgi_application()
