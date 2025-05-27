b344444e540d8a48c4375156575662d16f97e568
Ghalib92 <ghalibmohammad92@gmail.com>
2025-05-27 11:29:33 +0300

    ...

--- Atelier_Fashion/Atelier_Fashion/urls.py
+++ Atelier_Fashion/Atelier_Fashion/urls.py
@@ -24,6 +24,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('accounts/',include('Accounts.urls')),
 ]
 
 
