cf8b31f0563a137b70894dc0e984fc56a9ff3bd2
Ghalib92 <ghalibmohammad92@gmail.com>
2025-08-11 20:55:43 +0300

    login to add to cart

--- Atelier_Fashion/Atelier_Fashion/settings.py
+++ Atelier_Fashion/Atelier_Fashion/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
+from django.urls import reverse_lazy
 
 import os
 from dotenv import load_dotenv
@@ -35,7 +36,7 @@ LOW_STOCK_THRESHOLD = 10
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+LOGIN_URL = reverse_lazy('login')  # Best practice 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
