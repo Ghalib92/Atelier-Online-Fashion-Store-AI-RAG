96e0e866ca57ad9b318952dcd0744525991514cb
Mohaa <test@test.com>
2026-06-19 21:33:31 +0300

    Remove server-rendered templates, static and sample media
    Strip out all HTML templates, CSS/JS, uploaded sample media and leftover
    scaffolding scripts ahead of converting the project to a REST API.

--- Atelier_Fashion/pages/context_processors.py
+++
@@ -1,9 +0,0 @@
-from .models import Cart
-
-def cart_count(request):
-    if request.user.is_authenticated:
-        cart = Cart.objects.filter(user=request.user).first()
-        count = sum(item.quantity for item in cart.items.all()) if cart else 0
-    else:
-        count = 0
-    return {'cart_items_count': count}
